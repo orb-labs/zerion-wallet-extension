fbb6f63796b0a622e9834bf698a8536a659f9450
felimadu <fmadutsa@gmail.com>
2025-07-22 16:49:20 -0400

    make one_click_transactions_and_gas_abstraction default to true

--- src/shared/core/useIsOneClickTransactionsAndGasAbstractionEnabled.ts
+++ src/shared/core/useIsOneClickTransactionsAndGasAbstractionEnabled.ts
@@ -1,19 +1,9 @@
 import { useRemoteConfigValue } from 'src/modules/remote-config/useRemoteConfigValue';
-import { useMemo } from 'react';
-import _ from 'lodash';
 
 export function useIsOneClickTransactionsAndGasAbstractionEnabled() {
   const { data: oneClickTransactionsAndGasAbstraction } = useRemoteConfigValue(
     'one_click_transactions_and_gas_abstraction'
   );
 
-  return useMemo(() => {
-    // TODO: make oneClickTransactionsAndGasAbstraction default to false when it has been
-    // properly setup with the remote system
-    return (
-      (!_.isUndefined(oneClickTransactionsAndGasAbstraction) &&
-        oneClickTransactionsAndGasAbstraction) ||
-      true
-    );
-  }, [oneClickTransactionsAndGasAbstraction]);
+  return oneClickTransactionsAndGasAbstraction;
 }
